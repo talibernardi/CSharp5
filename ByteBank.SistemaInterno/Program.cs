941719d4cb644004856f58305a6f4ade85eefb0b
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-10-04 09:52:12 -0300

    primeiro commit

---
+++ ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteBank.Modelos;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2021, 11, 10);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+
+
+        }
+
+   
+
+    }
+}
